126672639cacedc39829fd37e439c62fb0df9162
MitchelRos <mrosado19@jaumebalmes.net>
2023-06-11 13:45:14 +0200

    ADD models, Edit settings

--- jaumebalmes_mitchel/jaumebalmes_mitchel/settings.py
+++ jaumebalmes_mitchel/jaumebalmes_mitchel/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'jaumebalmes_mitchel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DB_School',
+        'USER': 'postgres',
+        'PASSWORD': 'mitchel',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- jaumebalmes_mitchel/views_template_mitchel/models.py
+++ jaumebalmes_mitchel/views_template_mitchel/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# creacion modelos
+class Clases(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.CharField(max_length=100)
+    numberClass = models.IntegerField()
+    students = models.IntegerField()
+    classTutor = models.CharField(max_length=100)
+    classPresident = models.CharField(max_length=100)
+    description = models.TextField()
